0ec7513b85f861184665b27005de5ad3d255a5bc
Joao Manuel Aiveca Caseiro <jaiveca-@c2r10s2.42lisboa.com>
2023-01-28 19:15:17 +0000

    fourth

--- pipex.c
+++ pipex.c
@@ -6,21 +6,35 @@
 /*   By: jaiveca- <jaiveca-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 13:08:00 by jaiveca-          #+#    #+#             */
-/*   Updated: 2023/01/26 18:22:52 by jaiveca-         ###   ########.fr       */
+/*   Updated: 2023/01/28 16:00:24 by jaiveca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	cmd_parsing_pipex()
+void	cmd_parsing_pipex(char **envp)
 {
-	
+	char *envp_path;
+
+	while (envp[i])
+	{
+		ft_printf("%s\n", envp[i]);
+		i++;
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	int	infile_fd;
 	int	outfile_fd;
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		ft_printf("%s\n", envp[i]);
+		i++;
+	}
 
 	if (argc > 4)
 	{
